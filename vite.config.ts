4e867627da56b6570defc591274c4b5e97c26e72
namatedev <dev.tech@namategroup.com>
2025-07-15 13:21:33 +0100

    added test if connected to liferay

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,24 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
-    port: 8080,
+    proxy: {
+      '/api': {
+        target: process.env.VITE_REACT_APP_LIFERAY_BASE_URL || 'https://keycloak-security.apps.ocp4.namategroup.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, options) => {
+          proxy.on('error', (err, req, res) => {
+            console.log('proxy error', err);
+          });
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
+      }
+    }
   },
   plugins: [
     react(),
